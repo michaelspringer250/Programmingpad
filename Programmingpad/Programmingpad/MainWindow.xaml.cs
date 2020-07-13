e8b5bfcd95d831d888a7c5ed3693ae630e936960
Alian2018 <SydneyNinh@gmail.com>
2020-07-13 18:31:23 -0500

    Add event for drag and drop

--- Programmingpad/Programmingpad/MainWindow.xaml.cs
+++ Programmingpad/Programmingpad/MainWindow.xaml.cs
@@ -24,5 +24,31 @@ namespace Programmingpad
         {
             InitializeComponent();
         }
+
+        private void weapon_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Image i = (Image)sender;
+            /// passing the weapon name to the Drag and Drop event
+            DataObject dataObject = new DataObject(i.Name);
+            System.Windows.DragDrop.DoDragDrop(i, dataObject, DragDropEffects.Move);        
+        }
+
+        private void Bay_Drop(object sender, DragEventArgs e)
+        {
+            /// Mouse drop into the Bay image area
+            /// Pull the original click weapon from event
+            String weapon = (String)e.Data.GetData(typeof(String));
+        }
+
+        private void LeftWing_Drop(object sender, DragEventArgs e)
+        {
+            
+        }
+
+        private void RightWing_Drop(object sender, DragEventArgs e)
+        {
+            
+        }
     }
+    
 }
