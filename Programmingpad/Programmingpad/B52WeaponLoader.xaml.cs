1bc9a9eb0402bd02bede96202440f04aaf424733
michaelspringer250 <mspringer251@gmail.com>
2020-07-14 10:05:57 -0500

    MainWindow starts with the WeaponLoader page, implemented a basic button

--- Programmingpad/Programmingpad/B52WeaponLoader.xaml.cs
+++ Programmingpad/Programmingpad/B52WeaponLoader.xaml.cs
@@ -24,5 +24,10 @@ namespace Programmingpad
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Button clicked!");
+        }
     }
 }
