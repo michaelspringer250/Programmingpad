c0889b68097ac224c396db0d3d520f40bb11cc76
Sydney Ninh <Alian@MUP-DESKTOP>
2020-07-17 18:44:55 -0500

    Add border and some backgound color for GUI, generate template for WeaponStorage unit test

---
+++ Programmingpad/ProgrammingpadTests/WeaponStorageTests.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Programmingpad;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programmingpad.Tests
+{
+    [TestClass()]
+    public class WeaponStorageTests
+    {
+        [TestMethod()]
+        public void WeaponStorageTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ToStringTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void AddWeaponTest()
+        {
+            WeaponStorage actual = new WeaponStorage();
+            int expected = 1;
+            actual.AddWeapon(Weapon.ALCM);
+
+            Assert.AreEqual(actual.ALCM, expected);
+            Assert.ThrowsException<LoadErrorException>(() => actual.AddWeapon(Weapon.ALCM));
+        }
+
+        [TestMethod()]
+        public void CalcWeightTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ClearWeaponTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
